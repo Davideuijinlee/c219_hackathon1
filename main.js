73c65329ac233fbc0b52e64206bce59908d31cb1
Michelle Poole <michelle.poole.email@gmail.com>
2019-03-07 11:19:35 -0800

    player creation now runs at start stats are appended to individual containers

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).ready(startGame);
 var gameboard;
+var player1;
+var player2;
+var players = [];
 
 function startGame() {
     gameboard = new GameBoard;
+    player1 = new Player('Player 1', 1);
+    player2 = new Player('Player 2', 2);
+    players.push(player1);
+    players.push(player2);
 }
 
 // function clickInput(){
 
 // }
-
-var players = [];
-players.push(new Player('Player 1', 1));
-players.push(new Player('Player 2', 2));
-
-
